bf9b2dd22765f2270b4f5b91c2ac22554a35a958
Jaskirat KAUR <19kirat@gmail.com>
2022-04-22 23:30:45 -0400

    functionality is added to the Calculate Controller and the Calc View .

--- SquareRootCalculator/Controllers/CalculateController.cs
+++ SquareRootCalculator/Controllers/CalculateController.cs
@@ -1,12 +1,50 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace SquareRootCalculator.Controllers
 {
     public class CalculateController : Controller
     {
-        public IActionResult Index()
+        [HttpGet]
+        public IActionResult Calc()
         {
             return View();  
+        }
+        [HttpPost]
+        public IActionResult Calc(string FirstNumber, string SecondNumber)
+        {
+            try
+            {
+                if (int.Parse(FirstNumber) < 0 || int.Parse(SecondNumber) < 0)
+                {
+                    ViewBag.NegNumError = "Please Insert a Positive Number";
+                }
+                double firstNum = double.Parse(FirstNumber);
+                double secondNum = double.Parse(SecondNumber);
+
+                double firstNumSqrt = Math.Sqrt(firstNum);
+                double secondNumSqrt = Math.Sqrt(secondNum);
+
+                ViewBag.first = firstNum;
+                ViewBag.second = secondNum;
+                ViewBag.firstSqrt = firstNumSqrt;
+                ViewBag.secondSqrt = secondNumSqrt;
+
+
+
+            }
+            catch (ArgumentNullException ex)
+            {
+                string emptyValueError = ex.Message;
+                ViewBag.errorMessageEmpty = "Please input a number and try again";
+            }
+            catch(FormatException ex)
+            {
+                string alphabetError = ex.Message;
+                ViewBag.alphError = "Pleas input numbers only";
+            }
+           
+            return View();
         }
     }
 }
---
+++ SquareRootCalculator/Views/Calculate/Calc.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SquareRootCalculator.Views.Calculate
+{
+    public class CalcModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
